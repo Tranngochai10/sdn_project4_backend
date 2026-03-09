f7bd76f0dbd18b244cf7991ed8b589c04512fb50
haitnse182387@fpt.edu.vn <haitnse182387@fpt.edu.vn>
2026-03-09 10:28:54 +0700

    connect mongodb atlas

--- config/database.js
+++ config/database.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/SimpleQuiz');
+    // await mongoose.connect('mongodb://localhost:27017/SimpleQuiz');
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected successfully!');
   } catch (error) {
     console.error('MongoDB connection failed:', error);
